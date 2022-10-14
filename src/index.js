de3f2eb85c94de6252ad2a992f1e3f301baf947f
Traceyking121 <115085515+Traceyking121@users.noreply.github.com>
2022-10-14 10:21:20 -0400

    Current Temp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+function displayTemperature(response) {
+    console.log(response.data)
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let speedElement = document.querySelector( "#speed");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description
+    humidityElement.innerHTML = response.data.main.humidity;
+     speedElement.innerHTML = Math.round(response.data.wind.speed)
+}
+
 let apiKey =
     "115cc82b8a9ac9c633f8e4fdc3718a7f";
-  let city = "paris"
+  let city = "New York"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
