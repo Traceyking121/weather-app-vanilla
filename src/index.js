3783e1f19631bdfe26b192758439713f80c283d0
Traceyking121 <115085515+Traceyking121@users.noreply.github.com>
2022-10-14 09:28:59 -0400

    Fixed API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let apiKey =
-  "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=new york&appid=${apiKey}";
-  console.log(apiUrl)
+    "115cc82b8a9ac9c633f8e4fdc3718a7f";
+  let city = "paris"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+console.log(apiUrl);
